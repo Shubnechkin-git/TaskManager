467ca467225df4714eb79843148214318d9d0439
Даниил <danilka-shubnikov@mail.ru>
2024-03-22 03:19:17 +0400

    fix

--- frontend/src/panels/Quest/CreateQuest.js
+++ frontend/src/panels/Quest/CreateQuest.js
@@ -152,31 +152,6 @@ const CreateQuest = (props) => {
         }).catch(err => console.log(err));
     };
 
-    const [svgVisible, setSvgVisible] = useState(true);
-
-        function handleChange(e) {
-        const file = e.target.files[0];
-
-        const imageBuffer = file;
-        const reader = new FileReader();
-        reader.readAsDataURL(new Blob([imageBuffer], { type: 'image/jpeg' }));
-        reader.onload = () => {
-            console.log('card:', props.vk_id);
-            axios.post('https://192.168.0.106:3000/tasks/upload_image', { data: { file: reader.result, id: props.vk_id, id_task: props.id } })
-                .then(response => {
-                    console.log(response);
-                    setFile(response.data.image);
-                    // setFile(response.data.data[0].image);
-                    props.reload();
-                })
-                .catch(error => {
-                    console.error('Ошибка при загрузке файла:', error);
-                });
-            setSvgVisible(false);
-        };
-        setSvgVisible(false);
-    }
-
     return (
         <Styled>
             <div className='container_quest' >
@@ -188,19 +163,10 @@ const CreateQuest = (props) => {
                 <div className='container_input d-flex flex-column'>
                     <span className='fw-bold'>Добавьте изображения</span>
                     <div className='image d-flex justify-content-center align-items-center'>
-                        {svgVisible === true && props.image == null ? (
-                            <div className='d-flex justify-content-center w-100'>
-                                <File className='image_upload' onChange={handleChange}>
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z" fill="black" />
-                                        <path d="M11.9999 5.40002C12.3182 5.40002 12.6234 5.52645 12.8484 5.7515C13.0735 5.97654 13.1999 6.28176 13.1999 6.60002V10.8H17.3999C17.7182 10.8 18.0234 10.9265 18.2484 11.1515C18.4735 11.3765 18.5999 11.6818 18.5999 12C18.5999 12.3183 18.4735 12.6235 18.2484 12.8486C18.0234 13.0736 17.7182 13.2 17.3999 13.2H13.1999V17.4C13.1999 17.7183 13.0735 18.0235 12.8484 18.2486C12.6234 18.4736 12.3182 18.6 11.9999 18.6C11.6816 18.6 11.3764 18.4736 11.1514 18.2486C10.9263 18.0235 10.7999 17.7183 10.7999 17.4V13.2H6.5999C6.28164 13.2 5.97642 13.0736 5.75137 12.8486C5.52633 12.6235 5.3999 12.3183 5.3999 12C5.3999 11.6818 5.52633 11.3765 5.75137 11.1515C5.97642 10.9265 6.28164 10.8 6.5999 10.8H10.7999V6.60002C10.7999 6.28176 10.9263 5.97654 11.1514 5.7515C11.3764 5.52645 11.6816 5.40002 11.9999 5.40002Z" fill="white" />
-                                    </svg>
-                                </File>
-                            </div>
-                        ) : (
-                            <img src={file} className='img-fluid' />
-                        )
-                        }
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z" fill="black" />
+                            <path d="M11.9999 5.40002C12.3182 5.40002 12.6234 5.52645 12.8484 5.7515C13.0735 5.97654 13.1999 6.28176 13.1999 6.60002V10.8H17.3999C17.7182 10.8 18.0234 10.9265 18.2484 11.1515C18.4735 11.3765 18.5999 11.6818 18.5999 12C18.5999 12.3183 18.4735 12.6235 18.2484 12.8486C18.0234 13.0736 17.7182 13.2 17.3999 13.2H13.1999V17.4C13.1999 17.7183 13.0735 18.0235 12.8484 18.2486C12.6234 18.4736 12.3182 18.6 11.9999 18.6C11.6816 18.6 11.3764 18.4736 11.1514 18.2486C10.9263 18.0235 10.7999 17.7183 10.7999 17.4V13.2H6.5999C6.28164 13.2 5.97642 13.0736 5.75137 12.8486C5.52633 12.6235 5.3999 12.3183 5.3999 12C5.3999 11.6818 5.52633 11.3765 5.75137 11.1515C5.97642 10.9265 6.28164 10.8 6.5999 10.8H10.7999V6.60002C10.7999 6.28176 10.9263 5.97654 11.1514 5.7515C11.3764 5.52645 11.6816 5.40002 11.9999 5.40002Z" fill="white" />
+                        </svg>
                     </div>
                 </div>
                 <div className='container_input d-flex flex-column'>
